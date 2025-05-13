41b11dd88c4a63669d7b96331a91d0a5d1919d11
Bao Ho Gia <54936754+BaoHG1508@users.noreply.github.com>
2025-05-13 10:23:12 +0700

    chore: add comment

--- src/index.ts
+++ src/index.ts
@@ -835,7 +835,7 @@ export class StarknetIndexer {
     let continuationToken;
     let allEvents: EmittedEvent[] = [];
 
-    //TODO (Bao): Filter events by multiple contract addresses when supported
+    //TODO (Bao): Filter events by multiple contract addresses, event keys when supported
     for (const contractAddress of this.contractAddresses) {
       do {
         const response = await this.provider.getEvents({
