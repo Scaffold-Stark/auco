44f23064928c09b067274e322ed49f0d15b42fae
metalboyrick <rsulisthio@gmail.com>
2025-07-04 17:22:46 +0700

    feat: add sqlite handler

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,9 @@ export {
   EventHandler,
   ReorgHandler,
 } from './types/indexer';
+
+// Database handlers and types
+export { BaseDbHandler } from './utils/db/base-db-handler';
+export { PostgresDbHandler } from './utils/db/postgres-db-handler';
+export { SqliteDbHandler } from './utils/db/sqlite-db-handler';
+export type { BlockData, EventData, IndexerState } from './types/db-handler';
